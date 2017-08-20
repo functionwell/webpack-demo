308565ba33a852817236fc39b4c5689b828d295b
root <root@LAPTOP-69U98E1P.localdomain>
2017-08-20 10:56:20 +0800

    guide-output_management

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 // import './style.css';
 // import Logo from './webpack-logo.svg';
 // import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // 通过import引入lodash
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -15,6 +17,10 @@ function component() {
   // document.body.appendChild(myLogo);
 
   // console.log(Data);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
---
+++ src/print.js
@@ -0,0 +1,7 @@
+/**
+    管理输出
+*/
+
+export default function printMe() {
+    console.log('I get called from print.js!');
+}

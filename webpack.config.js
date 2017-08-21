abbda1adc2ab18baa4316b85f0074b7c88a4efdd
FANGKE <fangke@yuuwei.com>
2017-08-21 17:56:58 +0800

    guide-production

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const webpack = require('webpack');
 
+// // 配置分为两个文件，根据cli传参选择配置文件
+// module.exports = function(env) {
+//     return require(`./webpack.${env}.js`);
+// }
+
 module.exports = {
     entry: {
         app: './src/index.js',
@@ -14,6 +19,12 @@ module.exports = {
         hot: true
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
+        }),
         new ManifestPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
